faba38b2bc5da621c9333f995de3d2ef62654269
ibrahim-1987 <54317100+ibrahim-1987@users.noreply.github.com>
2021-09-15 22:38:17 -0400

    Add files via upload

---
+++ JokesWebApp/Models/Info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Info
+    {
+        public int ID { get; set; }
+
+        public string Education { get; set; }
+
+        public string Institution { get; set; }
+
+        public Info()
+        {
+
+        }
+    }
+}
